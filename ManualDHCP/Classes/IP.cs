867df315f3a3aa793e5d50db1c19f672c1bb6bf2
lange.yannik812 <lange.yannik812@gmail.com>
2017-09-28 23:57:22 +0200

    Added the contructor + profulls

--- ManualDHCP/Classes/IP.cs
+++ ManualDHCP/Classes/IP.cs
@@ -8,5 +8,45 @@ namespace ManualDHCP
 {
     class IP
     {
+        private string _id;
+        private string _netzadresse;
+        private string _subnetzmaske;
+        private int _prefix;
+        private int _anzHosts;
+
+        public IP(string id, string netzadresse, string subnetzmaske, int prefix, int anzhosts)
+        {
+            _id = id;
+            _netzadresse = netzadresse;
+            _subnetzmaske = subnetzmaske;
+            _prefix = prefix;
+            _anzHosts = anzHosts;
+        }
+
+        public string netzadresse
+        {
+            get { return _netzadresse; }
+            set { _netzadresse = value; }
+        }
+        public string subnetzmaske
+        {
+            get { return _subnetzmaske; }
+            set { _subnetzmaske = value; }
+        }
+        public int prefix
+        {
+            get { return _prefix; }
+            set { _prefix = value; }
+        }
+        public int anzHosts
+        {
+            get { return _anzHosts; }
+            set { _anzHosts = value; }
+        }
+        public string id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
     }
 }
